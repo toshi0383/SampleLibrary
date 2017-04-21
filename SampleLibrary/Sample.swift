03f0e0a85637f70f242572bfeb966b27b413b828
toshi0383 <t.suzuki326@gmail.com>
2017-04-21 12:02:22 +0900

    Initial

---
+++ SampleLibrary/Sample.swift
@@ -0,0 +1,19 @@
+//
+//  Sample.swift
+//  SampleLibrary
+//
+//  Created by 鈴木 俊裕 on 2017/04/21.
+//  Copyright © 2017 Toshihiro Suzuki. All rights reserved.
+//
+
+import Foundation
+
+public class Sample {
+    public init() {}
+    public var c = ""
+    public func run() {
+        let a = "a"
+        let b = "b"
+        self.c = "\(a), \(b)"
+    }
+}
---
+++ SampleLibrary/SampleLibrary.h
@@ -0,0 +1,19 @@
+//
+//  SampleLibrary.h
+//  SampleLibrary
+//
+//  Created by 鈴木 俊裕 on 2017/04/21.
+//  Copyright © 2017 Toshihiro Suzuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SampleLibrary.
+FOUNDATION_EXPORT double SampleLibraryVersionNumber;
+
+//! Project version string for SampleLibrary.
+FOUNDATION_EXPORT const unsigned char SampleLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleLibrary/PublicHeader.h>
+
+
